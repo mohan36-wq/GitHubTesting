45ee4a988bb248f332815af2fcfa3b7354fdc21c
mohan36-wq <52708818+mohan36-wq@users.noreply.github.com>
2020-08-08 14:16:35 +0530

    Adding check message

--- app/src/main/java/com/example/githubtesting/MainActivity.java
+++ app/src/main/java/com/example/githubtesting/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Working...", Toast.LENGTH_SHORT).show();
+        //Check 1
     }
 }
